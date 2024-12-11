70c7a4609e2b69c6d592dd872edfcd1039a2af76
Alireza Ahmadi <alirezaahmadi1308@gmail.com>
2024-12-12 02:07:04 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "vuetify/styles/main.sass"
 import "./core/styles/settings.scss"
-import router from "./router";
 import { registerPlugins } from "@/plugins"
 
-createApp(App).use(registerPlugins).use(router).mount("#app")
+createApp(App).use(registerPlugins).mount("#app")
