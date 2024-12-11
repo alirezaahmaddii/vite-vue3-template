630c18179e5b576c4d01b1724c766e10a264e8fa
Alireza Ahmadi <alirezaahmadi1308@gmail.com>
2024-12-12 02:21:00 +0330

    Fix run in mozilla firefox
    1

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "vuetify/styles/main.sass"
 import "./core/styles/settings.scss"
+import "./assets/styles/font.scss";
+import "./assets/styles/vutify.scss";
 import { registerPlugins } from "@/plugins"
 
 createApp(App).use(registerPlugins).mount("#app")
